67c7878f93dc6aebe745ebadf4f7efeea53f117a
Joshua Eliassaint <josheliass0502@gmail.com>
2024-03-19 13:30:06 -0400

    Removed Instructions, not needed

--- src/main/java/com/example/midterm/Application.java
+++ src/main/java/com/example/midterm/Application.java
@@ -11,7 +11,7 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1001, 618);
-        stage.setTitle("Midterm!");
+        stage.setTitle("Midterm");
         stage.setScene(scene);
         stage.show();
     }
